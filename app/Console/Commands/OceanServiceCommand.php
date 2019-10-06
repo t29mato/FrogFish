4f5a08e67c25af363d7997934b1ad5c4d38f9c74
t29mato <tomoya.matou@gmail.com>
2019-10-07 07:43:09 +0900

    スケジューラーのためのコマンド作成

---
+++ app/Console/Commands/OceanServiceCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use app\Services\OceanService;
+
+class OceanServiceCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'oceanService:execute';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '海況情報取得クローラー実行';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(OceanService $oceanService)
+    {
+        \Log::info('[開始] 海況情報取得');
+        $oceanService->execute();
+        \Log::info('[終了] 海況情報取得');
+    }
+}
