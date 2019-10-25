43ae8ec52ff1ac25df82ca8423b8ea48beed2ac1
t29mato <tomoya.matou@gmail.com>
2019-10-25 22:29:12 +0900

    テストコード作成 (TDDやってる風にコミット)

---
+++ tests/Controller/OceanControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Services;
+
+use Tests\TestCase;
+use App\Http\Controllers\OceanController;
+
+/**
+ * 透明度をintegerにする
+ */
+class OceanControllerTest extends TestCase
+{
+    /**
+     * @var OceanController
+     * @return void
+     */
+
+    private $oceanController;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->oceanController = new OceanController();
+    }
+
+    /**
+     * @dataProvider additionProvider透明度
+     */
+    public function test_透明度をintegerにする($transparency, $expected)
+    {
+        // PrivateメソッドをテストするためにReflection導入
+        // https://qiita.com/penton310/items/6b437061391016179631
+        $reflection = new \ReflectionClass($this->oceanController);
+        $transparencyCast2integerFunction = $reflection->getMethod('transparencyCast2integer');
+        $transparencyCast2integerFunction->setAccessible(true);
+
+        $actual = $transparencyCast2integerFunction->invoke($transparency);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function additionProvider透明度()
+    {
+        return [
+            ['-', '0'],
+            // ['3〜8m', '6'],
+            // ['10m', '10'],
+            // ['10〜12m', '11'],
+        ];
+    }
+}
