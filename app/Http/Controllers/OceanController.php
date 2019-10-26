a8675787571c4efa10f5033d0f47f331670a911e
t29mato <tomoya.matou@gmail.com>
2019-10-26 09:21:58 +0900

    境界値などテストデータ追加

--- app/Http/Controllers/OceanController.php
+++ app/Http/Controllers/OceanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Ocean;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\App;
 
 class OceanController extends Controller
